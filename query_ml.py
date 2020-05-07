34b51ad7af84766d3c8ac0e01e46e469411f34f3
Andrey Sugonyaev <sug.andre606@gmail.com>
2020-05-07 13:49:46 +0300

    Move stuff into functions

--- query_ml.py
+++ query_ml.py
@@ -1,64 +1,140 @@
+import argparse
 import os
+import re
+import numpy as np
 from query import Indexer
-from embedder import Embedder
-from scipy.spatial.distance import cosine, euclidean
+from embedder import Embedder, get_text_reduced
+from merge_operations import not_and_postings
+from scipy.spatial.distance import cosine
 
-#FIXME mb this should be in embedder.py, as it is actually its preprocessing
-def get_text(filename):
-    with open(filename, 'r')as f:
-        lines = [line.rstrip() for line in f]
-        text = ' '.join(x for x in lines if x != "")
-    return text
+def query_expand(query: str) -> str, str:
+    m = re.search(r" NOT", query)
+    if m:
+        q_pos = q[:m.start()]
+        q_neg = q[m.end():].strip('()')
+    else:
+        q_pos = query
+        q_neg = None
+    return q_pos, q_neg
+
+
+def query_reduce(query: str) -> str:
+    query = re.sub(r"NOT", "", query)
+    query = re.sub(r"[()]", "", query)
+    return query
+
+
+def batch_embed(embedder: Embedder, texts: [str], batch_size: int) -> np.ndarray:
+    embeddings = np.zeros((len(texts), 768))
+    
+    iters = len(texts)//batch_size
+    if  len(texts)%batch_size > 0:
+        iters += 1
+
+    for i in range(iters):
+        batch = texts[i*batch_size:(i+1)*batch_size]
+        emb_batch = embedder.embed(batch)
+        embeddings[i*batch_size:(i+1)*batch_size] = emb_batch
+    return embeddings
+
+
+def arg_parse() -> argparse.Namespace:
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Querying with ML")
+    parser.add_argument(
+        "--root",
+        dest="root",
+        help="Lyrics root directory",
+        default="lyrics/",
+        type=str,
+    )
+    parser.add_argument(
+        "--index", dest="index", help="Index file", default="index", type=str
+    )
+    parser.add_argument(
+        "--q",
+        dest="query",
+        help="Query string. Syntax: 'word1 word2 NOT(word3 word4)'",
+        default="",
+        type=str,
+    )
+    parser.add_argument(
+        "--L0",
+        dest="l0_count",
+        help="How many hits from L0 are reranked with ML",
+        default=100,
+        type=int,
+    )
+    parser.add_argument(
+        "--L1",
+        dest="l1_count",
+        help="How many hits to show",
+        default=20,
+        type=int,
+    )
+    parser.add_argument(
+        "--bs",
+        dest="batch_size",
+        help="Batch size to use in L1, decrease if GPU rans out of memory",
+        default=100,
+        type=int,
+    )
+    return parser.parse_args()
 
 
 def main():
-    #TODO clean up
-    
-    root = "lyrics/"
+    args = arg_parse()
     docs = [
-        dir+'/'+f 
-        for dir in os.listdir(root) 
-        for f in os.listdir(root+dir)
+        os.path.join(dir, f)
+        for dir in os.listdir(args.root) 
+        for f in os.listdir(os.path.join(args.root, dir))
     ]
     docs = sorted(docs)
     
-    index = Indexer(docs, "index", root)
+    index = Indexer(docs, args.index, args.root)
     embedder = Embedder()
-    l0_count=20
     
     #L0
-    #TODO expand free-text query to OR, add support for NOT 
-    q = "nothing OR else OR matters"
-    tokens = q.split()
-    hits = index.query_boolean(tokens)
-    hits = sorted(hits, key=lambda item: item[1], reverse=True)
+    q_pos, q_neg = query_expand(args.query)
+    #Get all OR-ed tokens
+    q_pos_expand = re.sub(r" ", " OR ", q_pos)
+    hits = index.query_boolean(q_pos_expand.split())
+    #Remove all NOT-ed tokens
+    if q_neg:
+        for token in q_neg.split():
+            term = index.stemmer.stem(token)
+            not_posting = index.tfidf(index.index[term])
+            hits = not_and_postings(not_posting, hits)
     
-    doc_ids = [x[0] for x in hits[:l0_count]]
-    filenames = [root + docs[i] for i in doc_ids]
-    texts = [get_text(x) for x in filenames]
+    if not hits:
+        print("nothing found")
+        return
+    
+    hits = sorted(hits, key=lambda item: item[1], reverse=True)
+    hits = hits[:args.l0_size]
     
     #L1
-    q = "nothing else matters"  #FIXME remove ORs
-    embeddings = embedder.embed(texts)
-    query_emb = embedder.embed([q])[0]
+    doc_ids = [x[0] for x in hits]
+    filenames = [os.path.join(args.root, docs[i]) for i in doc_ids]
+    texts = [get_text_reduced(x, maxlen=512) for x in filenames]
     
-    dist_euq = [euclidean(query_emb, e) for e in embeddings]
+    #Embed in batches if GPU memory is small
+    if args.batch_size >= args.l0_count:
+        embeddings = embedder.embed(texts)
+    else:
+        embeddings = batch_embed(embedder, texts, args.batch_size)
+    query_emb = embedder.embed([q_pos])[0]
     dist_cos = [cosine(query_emb, e) for e in embeddings]
-    idx_euq = np.argsort(dist_euq)
     idx_cos = np.argsort(dist_cos)
     
     #Render
-    indexes = idx_cos
-    resorted = [doc_ids[i] for i in indexes]
-
-    for i,id in enumerate(resorted):
+    q_red = query_reduce(args.query)
+    resorted = [doc_ids[i] for i in idx_cos]
+    for i,id in enumerate(resorted[:args.l1_size]):
         print("\n{}:".format(i))
-        index.render_file(q.split(), docs[id])
-
-        orig_pos = indexes[i]
-        print("\tOriginal rank: ", orig_pos)
-        print("\ttf-idf = ", hits[orig_pos][1])
-        print("\tcos-sim = ", 1-dist_cos[orig_pos])
+        index.render_file(q_red.split(), docs[id])
+        orig_pos = idx_cos[i]
+        print("\tL0 rank = {}; tf-idf = {:.3f}; cos-sim = {:.3f}".format(orig_pos, hits[orig_pos][1], 1-dist_cos[orig_pos]))
 
 if __name__ == "__main__":
     main()
